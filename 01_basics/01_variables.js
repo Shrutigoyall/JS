cdd161df72bc4f997492a471ccd62068fa64fa90
Shrutigoyall <117890796+Shrutigoyall@users.noreply.github.com>
2024-07-06 09:34:31 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 122344
+let accountEmail = "shruti@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur" 
+let accountState;
+// accountId = 2 // not allowed
+
+accountEmail = "bhavya@gmail.com"
+accountPassword = "12356"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all JS code as new version
+
+// alert(3 + 3) // we're using nodejs not browser
+
+console.log(3 + 3)
+
+console.log("shruti")
+
+let name = "shruti"
+let age = 21
+let isLoggedIn = false
+
+
+// number -> 2 to power 53
+// bigint
+// string  -> ""
+// boolean ->  true/false
+// null -> standalone value
+// undefined -> 
+// symbol -> unique
+
+
+
+// object
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null); //object
+
